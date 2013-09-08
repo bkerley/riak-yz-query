9e90d6b8803ba2fe3193a1a92c0453fa4b12dfe1
Bryce Kerley <bkerley@brycekerley.net>
2013-09-08 16:17:22 -0400

    implement query builder where clauses

--- lib/riak/yz/query/query_builder.rb
+++ lib/riak/yz/query/query_builder.rb
@@ -5,6 +5,31 @@ module Riak
         def initialize(bucket)
           @bucket = bucket
         end
+
+        def where(opts)
+          where_clauses.merge! opts
+
+          self
+        end
+
+        def keys
+          results['docs'].map{|d| d['_yz_rk']}
+        end
+
+        def to_yz_query
+          where_clauses.map do |k,v|
+            "#{k}:#{v}"
+          end.join ' AND '
+        end
+
+        private
+        def results
+          @results ||= @bucket.client.search @bucket.name, to_yz_query
+        end
+
+        def where_clauses
+          @where_clauses ||= Hash.new
+        end
       end
     end
   end
