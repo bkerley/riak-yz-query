2d4a8643543d62909f12e28d941c6c6d47c8a0be
Bryce Kerley <bkerley@brycekerley.net>
2013-09-08 16:42:09 -0400

    more thorough query testing

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -10,13 +10,24 @@ class IntegrationTest < TestCase
       q = @bucket.query.where(name_t: '*drew*')
       assert q
       refute_empty q.keys
-      assert_includes q.keys, 'PtgA5YsxWpSg7RzTY2eJVJ81hDQ'
+      ['Z0tsBudxTQp50pBlTNeBw6CtwZx',
+       'PtgA5YsxWpSg7RzTY2eJVJ81hDQ',
+       'OL1quOfOKiYEmxYsqvjf9cyRmH3'].each do |k|
+        assert_includes q.keys, k
+      end
     end
 
     should 'perform multi-term AND queries' do
       q = @bucket.query.where(name_t: '*drew*', title_t: '*engineer*')
       assert q
       refute_empty q.keys
+      ['Z0tsBudxTQp50pBlTNeBw6CtwZx',
+       'PtgA5YsxWpSg7RzTY2eJVJ81hDQ'].each do |k|
+        assert_includes q.keys, k
+      end
+      ['OL1quOfOKiYEmxYsqvjf9cyRmH3'].each do |k|
+        refute_includes q.keys, k
+      end
     end
 
     should 'perform multi-term OR queries'
