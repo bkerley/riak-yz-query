bdbde7a5432b017729cc52a5853d0902fe588252
Bryce Kerley <bkerley@brycekerley.net>
2013-09-08 16:39:30 -0400

    eventually test or queries

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -13,11 +13,13 @@ class IntegrationTest < TestCase
       assert_includes q.keys, 'PtgA5YsxWpSg7RzTY2eJVJ81hDQ'
     end
 
-    should 'perform multi-term queries' do
+    should 'perform multi-term AND queries' do
       q = @bucket.query.where(name_t: '*drew*', title_t: '*engineer*')
       assert q
       refute_empty q.keys
     end
+
+    should 'perform multi-term OR queries'
 
     should 'perform range queries'
     should 'perform single-term queries with pagination controls'
