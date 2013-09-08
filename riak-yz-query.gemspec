dc390484c967a6876bd4aa86bd94b4553e0d30a4
Bryce Kerley <bkerley@brycekerley.net>
2013-09-08 15:41:55 -0400

    fix a compat issue with minitest

--- riak-yz-query.gemspec
+++ riak-yz-query.gemspec
@@ -20,7 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
-  spec.add_development_dependency "minitest", "~> 5.0.7"
+  spec.add_development_dependency "minitest", "~> 4.7"
   spec.add_development_dependency "shoulda-context", "~> 1.1.5"
 
   spec.add_dependency 'riak-client'
--- test/helper.rb
+++ test/helper.rb
@@ -1,9 +1,28 @@
 require 'rubygems'
 require 'bundler/setup'
-
 require 'minitest/autorun'
+
 require 'shoulda/context'
 
 class TestCase < MiniTest::Unit::TestCase
   include ShouldaContextLoadable
+
+  def client
+    @client ||= Riak::Client.new pb_port: 10017
+  end
+
+  def bucket_name
+    return @bucket_name if defined? @bucket_name
+    @bucket_name = "riak-yz-query-#{rand(36**10).to_s(36)}"
+    $stderr.puts "Using bucket name #{@bucket_name.inspect}"
+  end
+
+  def bucket
+    return @bucket if defined? @bucket
+
+    @bucket = client.bucket bucket_name
+  end
+
+  
 end
+
