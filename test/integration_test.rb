0a5347dad484ed714d5c7c0b2a68316becfb7100
Bryce Kerley <bkerley@brycekerley.net>
2013-09-08 15:42:14 -0400

    stub first integration test

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -2,7 +2,16 @@ require 'helper'
 
 class IntegrationTest < TestCase
   context 'riak-yz-query with some data' do
-    should 'perform single-term queries'
+    setup do
+      @bucket = client.bucket 'user'
+    end
+
+    should 'perform single-term queries' do
+      results = @bucket.query.where(name_t: '*drew*')
+      assert results
+      refute_empty results
+    end
+
     should 'perform multi-term queries'
     should 'perform range queries'
     should 'perform single-term queries with pagination controls'
