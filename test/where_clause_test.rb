1de5616a7831ffde0508ebdc2b53f14a5e538891
Bryce Kerley <bkerley@brycekerley.net>
2013-09-08 17:56:49 -0400

    where clause tests pass

--- test/where_clause_test.rb
+++ test/where_clause_test.rb
@@ -1,23 +1,25 @@
 require 'helper'
 
 class WhereClauseTest < TestCase
+  WhereClause = Riak::YzQuery::WhereClause
+
   context 'a WhereClause' do
     should 'merge a hash and a hash' do
       c = WhereClause.new asdf: 'jkl'
       c_merge = c.consume qwe: 'rty'
-      assert_equal 'asdf:jkl and qwe:rty', c_merge.to_yz_query
+      assert_equal 'asdf:jkl AND qwe:rty', c_merge.to_yz_query
     end
 
     should 'merge a hash and a string' do
       c = WhereClause.new asdf: 'jkl'
       c_merge = c.consume 'qwe:rty'
-      assert_equal 'asdf:jkl and qwe:rty', c_merge_to_yz_query
+      assert_equal '(asdf:jkl) AND qwe:rty', c_merge.to_yz_query
     end
 
     should 'merge a string and a string' do
       c = WhereClause.new "bryce:hello"
       c_merge = c.consume "hello:to_u"
-      assert_equal "(bryce:hello) AND hello:to_u"
+      assert_equal "(bryce:hello) AND hello:to_u", c_merge.to_yz_query
     end
 
     should 'escape an array into a string'
